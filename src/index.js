48741c3b926a9aee05424bec5d6934c4b9bdb40b
Nkoebotse Elliot Sekgobela <136261850+nkoebotse@users.noreply.github.com>
2024-10-25 10:45:06 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Updated import
+import App from './App'; // Adjust as needed
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
